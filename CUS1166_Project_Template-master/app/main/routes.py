711ec68c004c66fecf3eb3d23ba8461a50b0ef4e
Navneet Singh <Nsingh1499@gmail.com>
2019-12-04 20:31:35 -0500

    working product

--- CUS1166_Project_Template-master/app/main/routes.py
+++ CUS1166_Project_Template-master/app/main/routes.py
@@ -1,26 +1,29 @@
-from flask import render_template,  redirect, url_for
+from flask import render_template, redirect, url_for
 from app.main import bp
 from app import db
-from app.main.forms import TaskForm
-from app.models import Task
+from app.main.forms import TaskForm, AppointmentForm
+from app.models import Task, Appointment
 
 
 # Main route of the applicaitons.
-@bp.route('/', methods=['GET','POST'])
+from sqlalchemy import desc
+
+
+@bp.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("main/index.html")
 
 
 #
 #  Route for viewing and adding new tasks.
-@bp.route('/todolist', methods=['GET','POST'])
+@bp.route('/todolist', methods=['GET', 'POST'])
 def todolist():
     form = TaskForm()
 
     if form.validate_on_submit():
         # Get the data from the form, and add it to the database.
         new_task = Task()
-        new_task.task_desc =  form.task_desc.data
+        new_task.task_desc = form.task_desc.data
         new_task.task_status = form.task_status_completed.data
 
         db.session.add(new_task)
@@ -31,14 +34,13 @@ def todolist():
 
     todo_list = db.session.query(Task).all()
 
-    return render_template("main/todolist.html",todo_list = todo_list,form= form)
+    return render_template("main/todolist.html", todo_list=todo_list, form=form)
 
 
 #
 # Route for removing a task
-@bp.route('/appointment/remove/<int:appointment_id>', methods=['GET','POST'])
+@bp.route('/todolist/remove/<int:task_id>', methods=['GET', 'POST'])
 def remove_task(task_id):
-
     # Query database, remove items
     Task.query.filter(Task.task_id == task_id).delete()
     db.session.commit()
@@ -49,7 +51,7 @@ def remove_task(task_id):
 #
 # Route for editing a task
 
-@bp.route('/todolist/edit/<int:task_id>', methods=['GET','POST'])
+@bp.route('/todolist/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
     form = TaskForm()
     print(form.validate_on_submit())
@@ -57,7 +59,7 @@ def edit_task(task_id):
         # Get the data from the form, and add it to the database.
 
         current_task = Task.query.filter_by(task_id=task_id).first_or_404()
-        current_task.task_desc =  form.task_desc.data
+        current_task.task_desc = form.task_desc.data
         current_task.task_status = form.task_status_completed.data
 
         db.session.add(current_task)
@@ -69,7 +71,83 @@ def edit_task(task_id):
     current_task = Task.query.filter_by(task_id=task_id).first_or_404()
 
     # update the form model in order to populate the html form.
-    form.task_desc.data =     current_task.task_desc
+    form.task_desc.data = current_task.task_desc
     form.task_status_completed.data = current_task.task_status
 
-    return render_template("main/todolist_edit_view.html",form=form, task_id = task_id)
+    return render_template("main/todolist_edit_view.html", form=form, task_id=task_id)
+
+
+@bp.route('/ScheduleAppointment', methods=['GET', 'POST'])
+def Schedule():
+    form = AppointmentForm()
+    if form.validate_on_submit():
+        new_appointment = Appointment()
+
+        new_appointment.customer_name = form.customer_name.data
+        new_appointment.appointment_title = form.appointment_title.data
+        new_appointment.appointment_date = form.appointment_date.data
+        new_appointment.appointment_time = form.start_time.data
+        new_appointment.appointment_duration = form.duration_of_appointment.data
+        new_appointment.appointment_location = form.location.data
+        new_appointment.notes = form.notes.data
+        new_appointment.appointment_status = form.appointment_status_completed.data
+
+        db.session.add(new_appointment)
+        db.session.commit()
+        return redirect(url_for('main.view_schedule'))
+    return render_template('main/schedule_appointment.html', title='Schedule an appointment', form=form)
+
+
+@bp.route('/Schedule/edit/<int:appointment_id>', methods=['GET', 'POST'])
+def Update_Schedule(appointment_id):
+    form = AppointmentForm()
+    if form.validate_on_submit():
+        current_appointment = Appointment.query.filter_by(appointment_id=appointment_id).first_or_404()
+        current_appointment.customer_name = form.customer_name.data
+        current_appointment.appointment_title = form.appointment_title.data
+        current_appointment.appointment_date = form.appointment_date.data
+        current_appointment.appointment_time = form.start_time.data
+        current_appointment.appointment_duration = form.duration_of_appointment.data
+        current_appointment.appointment_location = form.location.data
+        current_appointment.appointment_status = form.appointment_status_completed.data
+        current_appointment.notes = form.notes.data
+
+        db.session.add(current_appointment)
+        db.session.commit()
+
+        return redirect(url_for('main.view_schedule'))
+
+    current_appointment = Appointment.query.filter_by(appointment_id=appointment_id).first_or_404()
+
+    form.customer_name.data = current_appointment.customer_name
+    form.appointment_title.data = current_appointment.appointment_title
+    form.appointment_date.data = current_appointment.appointment_date
+    form.start_time.data = current_appointment.appointment_time
+    form.duration_of_appointment.data = current_appointment.appointment_duration
+    form.location.data = current_appointment.appointment_location
+    form.appointment_status_completed.data = current_appointment.appointment_status
+    form.notes.data = current_appointment.notes
+
+    return render_template('main/schedule_appointment.html', title='Schedule an appointment', form=form)
+
+
+@bp.route('/Schedule/remove/<int:appointment_id>', methods=['GET', 'POST'])
+def remove_schedule(appointment_id):
+    # Query database, remove items
+    Appointment.query.filter(Appointment.appointment_id == appointment_id).delete()
+    db.session.commit()
+
+    return redirect(url_for('main.view_schedule'))
+
+
+@bp.route('/ViewAppointments', methods=['GET', 'POST'])
+def view_schedule():
+    schedule = db.session.query(Appointment).order_by(desc(Appointment.appointment_time)).all()
+
+    return render_template('main/display_schedules.html', schedule=schedule)
+
+
+@bp.route('/view_appointment_info/<int:appointment_id>', methods=['GET', 'POST'])
+def view_apppointment_info(appointment_id):
+    current_appointment = Appointment.query.filter_by(appointment_id=appointment_id).first_or_404()
+    return render_template('main/display_appointment_info.html', current_appointment=current_appointment)
--- CUS1166_Project_Template-master/app/models.py
+++ CUS1166_Project_Template-master/app/models.py
@@ -1,46 +1,18 @@
 
 #from flask import url_for
 from app import db
-from flask_sqlalchemy import SQLAlchemy
 
-class add(db.Model):
-    __tablename__ = 'add'
-    id = db.Column(db.Integer, primary_key=True)
-
-
-
-class delete(db.Model):
-    __tablename__ = 'delete'
-    id = db.Column(db.Integer, primary_key=True)
-
-class edit(db.Model):
-    __tablename__ = 'edit'
-    id = db.Column(db.Integer, primary_key=True)
-
-# class Attendappointment(db.Model):
-#     __tablename__ = 'attend'
-#     user_id = db.Column(db.Integer, db.ForeignKey('Customers.id'), primary_key=True)
-#     appointment_id = db.Column(db.Integer, db.ForeignKey('Appointment.id'), primary_key=True)
-
-# class Customers(db.Model):
-#     __tablename__ = 'customer'
-#     id = db.Column(db.Integer, primary_key=True)
-#     address = db.Column(db.String)
-#     name = db.Column(db.String)
-#     notes = db.Column(db.String)
-#     appointment = db.relationship('customers', secondary ='attend',back_populates= 'appointments' )
-#
 
 class Appointment(db.Model):
-    __tablename__ = 'appointments'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    start = db.Column(db.DateTime)
-    end = db.Column(db.DateTime)
-    location = db.Column(db.String)
-    attending_user = db.relationship('Customers', secondary='attend', back_populates="appointments")
-    appointment_id = db.relationship('Appointment', secondary = 'customer', back_populates = '' )
-
+    appointment_id = db.Column(db.Integer, primary_key=True)
+    appointment_title = db.Column(db.String(120), index=True)
+    appointment_date = db.Column(db.Date, index=True)
+    appointment_time = db.Column(db.Time, index=True)
+    appointment_duration = db.Column(db.String, index=True)
+    appointment_location = db.Column(db.String(120), index=True)
+    customer_name = db.Column(db.String(120), index=True)
+    notes = db.Column(db.String(120), index=True)
+    appointment_status = db.Column(db.String(10), index=True)
 
 class Task(db.Model):
 
